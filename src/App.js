3a197714595384dda68cbb8b9e594aa959b4747d
BrunoBalmat <111462798+BrunoBalmat@users.noreply.github.com>
2024-07-04 17:19:46 -0300

    inicio do projeto animes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import HamburgerMenu from './components/Hamburger';
 
 function App() {
+
+  const [populares, setPopulares] = useState();
+
+  useEffect(() => {
+    axios.get(`https://kitsu.io/api/edge/anime?page%5Blimit%5D=5&sort=-average_rating`)
+      .then(function (response) {
+        setPopulares(response.data.data);
+      })
+      .catch(function (error) {
+        console.error("Erro ao buscar", error);
+      });
+  }, []);
+
+  console.log(populares, 'populares');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HamburgerMenu/>
+      <div>
+        <header className="App-header">
+          <a className='linkhome' href='/'><img src='/logo.png' alt='logo' /></a>
+          <input />
+        </header>
+        <div className='displayPopulares'>
+          <h2>Animes Mais Populares</h2>
+          <div className='imagensPopulares'>
+            {populares?.map(personagem => {
+              return (
+                <div className='display5animes'>
+                  <img src={personagem?.attributes?.posterImage?.small} alt="anime" />
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
